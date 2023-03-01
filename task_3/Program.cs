b3b2f6aefbfe62a6ce7d5057bd87f82b95bf4226
Yanina <moonspb75@mail.ru>
2023-03-01 10:58:56 +0300

    задача 42

---
+++ task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+
+
+int num = Convert.ToInt32(Console.ReadLine());
+string arr  = "";
+
+while (num > 0)
+{
+    arr = arr + Convert.ToString(num % 2);
+    num = num / 2;
+}
+
+ char[] charArray = arr.ToCharArray();
+Array.Reverse(charArray);
+Console.WriteLine(string.Join("",charArray));
